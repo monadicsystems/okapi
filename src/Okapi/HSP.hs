8bb92d748dc776e37d6fc64fa65684dd6eecfce1
Rashad Gover <rashad.sasaki@gmail.com>
2022-10-05 06:29:20 +0000

    Add directory structure for implementing file-based routing for HSPs

--- src/Okapi/HSP.hs
+++ src/Okapi/HSP.hs
@@ -5,6 +5,7 @@ module Okapi.HSP where
 import qualified Control.Monad as Monad
 import qualified Control.Monad.IO.Class as IO
 import qualified Data.Attoparsec.Text as Atto
+import Data.Functor ((<&>))
 import qualified Data.List as List
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
@@ -16,6 +17,45 @@ import qualified Language.Haskell.TH.Quote as TH
 import qualified Okapi
 import qualified System.Directory as Directory
 import qualified System.FilePath.Posix as FilePath
+
+
+data DirTree = Dir Text.Text [DirTree] | File Text.Text Text.Text
+  deriving (Show, Eq)
+
+buildDirTree' root = do
+  executableDir <- IO.liftIO Directory.getCurrentDirectory
+  dirTree <- buildDirTree root
+  IO.liftIO $ Directory.setCurrentDirectory executableDir
+  pure dirTree
+
+buildDirTree :: IO.MonadIO m => FilePath -> m DirTree
+buildDirTree root = do
+  rootContents <- IO.liftIO $ Directory.listDirectory root
+  dirTrees <- Monad.forM rootContents $ \content -> do
+    case FilePath.takeExtension content of
+      ""  -> do
+        IO.liftIO $ Directory.setCurrentDirectory root
+        buildDirTree content
+      ext -> pure $ File (Text.pack $ FilePath.dropExtension content) (Text.pack ext)
+  pure $ Dir (Text.pack root) dirTrees
+
+getFullPaths :: IO.MonadIO m => FilePath -> m [[FilePath]]
+getFullPaths root = do
+  fullPathsWithRoot <- loop root
+  let
+    rootSplit :: [FilePath] = FilePath.splitPath root
+    splitFullPathsWithRoot :: [[FilePath]] = map FilePath.splitPath fullPathsWithRoot
+    splitFullPathsWithoutRoot :: [[FilePath]] = map (\splitFullPathWithRoot -> splitFullPathWithRoot List.\\ rootSplit) splitFullPathsWithRoot
+  pure splitFullPathsWithoutRoot
+  where
+    loop :: IO.MonadIO m => FilePath -> m [FilePath]
+    loop root' = do
+      rootContents <- IO.liftIO $ Directory.listDirectory root'
+      tree <- Monad.forM rootContents $ \content -> do
+        case FilePath.takeExtension content of
+          "" -> loop $ root' FilePath.</> content
+          _ -> pure [root' FilePath.</> content]
+      pure $ concat tree
 
 {-| This quasiquoter generates a parser using HSP files.
 It parses a root directory and does everything from there.
@@ -64,6 +104,7 @@ generateParser' hspDirectory = do
       pretty = TH.pprint someExp
   pure pretty
 
+-- TODO: DO REMAINDER OF WORK HERE
 generateParser :: FilePath -> TH.Q TH.Exp
 generateParser hspDirectory = do
   currentDirectory <- IO.liftIO $ Directory.getCurrentDirectory
@@ -71,29 +112,33 @@ generateParser hspDirectory = do
   fullPaths <- IO.liftIO $ getFullPaths hspDirAbsPath
   fullPathsWithExp :: [([FilePath], TH.Exp)] <- IO.liftIO $ Monad.forM fullPaths $ \fullPath -> do
     fileContents <- readFile $ hspDirAbsPath FilePath.</> (FilePath.joinPath fullPath)
-    let indentedFileContents = unlines $ map (\line -> "  " <> line) $ lines fileContents
-        doBlock = "do/n" <> indentedFileContents
-    case Meta.parseExp doBlock of
+    let
+      indentedFileContents = unlines $ map (\line -> "  " <> line) $ lines fileContents
+      doBlock = "do/n" <> indentedFileContents
+    case parseExp doBlock of
       Left _ -> error "The do block isn't formatted correctly!"
-      Right exp -> pure (fullPath, exp)
-  let (_, someExp) = head fullPathsWithExp
-      pretty = TH.pprint someExp
-  pure someExp
+      Right exp@(TH.DoE stmts) -> do
+        let
+          prefixStmts :: [TH.Stmt] = fullPath <&> \path ->
+            if isPathParam $ FilePath.dropExtension path
+            then undefined
+            else undefined
+        pure (fullPath, exp)
+      _ -> error "This isn't a do block!"
+  undefined
 
-getFullPaths :: IO.MonadIO m => FilePath -> m [[FilePath]]
-getFullPaths root = do
-  fullPathsWithRoot <- loop root
-  let
-    rootSplit :: [FilePath] = FilePath.splitPath root
-    splitFullPathsWithRoot :: [[FilePath]] = map FilePath.splitPath fullPathsWithRoot
-    splitFullPathsWithoutRoot :: [[FilePath]] = map (\splitFullPathWithRoot -> splitFullPathWithRoot List.\\ rootSplit) splitFullPathsWithRoot
-  pure splitFullPathsWithoutRoot
-  where
-    loop :: IO.MonadIO m => FilePath -> m [FilePath]
-    loop root' = do
-      rootContents <- IO.liftIO $ Directory.listDirectory root'
-      tree <- Monad.forM rootContents $ \content -> do
-        case FilePath.takeExtension content of
-          "" -> loop $ root' FilePath.</> content
-          _ -> pure [root' FilePath.</> content]
-      pure $ concat tree
+isPathParam :: FilePath -> Bool
+isPathParam (h:t) =
+  if h == '['
+  then
+    if (dropWhile (']' /=) t) == "]"
+    then True
+    else False
+  else False
+isPathParam _ = False
+
+isDirectory :: FilePath -> Bool
+isDirectory filePath =
+  if (dropWhile ('/' /=) filePath) == "/"
+    then True
+    else False
