809c8a98243c9ae5cc1c387b043ed1fe802c1843
Rashad Gover <rashad.sasaki@gmail.com>
2022-10-11 05:15:55 +0000

    Clean up code

--- src/Okapi/HSP.hs
+++ src/Okapi/HSP.hs
@@ -31,7 +31,7 @@ Must use trailing slash!
 hsp :: TH.QuasiQuoter
 hsp =
   TH.QuasiQuoter
-    { TH.quoteExp  = (\fp -> (normalizeDirTree "" <$> buildDirTree' fp) >>= buildExpr) ,
+    { TH.quoteExp  = (\fp -> (normalizeDirTree "" <$> buildDirTree' fp) >>= buildExpr),
       TH.quotePat  = undefined,
       TH.quoteType = undefined,
       TH.quoteDec  = undefined
@@ -65,8 +65,8 @@ buildExpr (File name ext) = case ext of
         error "The do block isn't formatted correctly or this isn't even Haskell!"
       Right (TH.DoE stmts) -> pure $ TH.DoE $ prefixStmts <> stmts
       _ -> error "This isn't a do block!"
-  ".html" -> undefined 
-  ".js" -> undefined  
+  ".html" -> undefined
+  ".js" -> undefined
   ".css" -> undefined
   _ -> undefined
 buildExpr (Dir name dirTrees) = do
@@ -135,82 +135,3 @@ removeBrackets _     = error "No Brackets to remove dude!"
 
 parseExp :: String -> Either String TH.Exp
 parseExp = either Left (Right . Meta.toExp) . Meta.parseResultToEither . Exts.parseExpWithMode (Exts.defaultParseMode {Exts.extensions = [Exts.EnableExtension Exts.TypeApplications, Exts.EnableExtension Exts.BlockArguments]})
-
-{-
-getFullPaths :: IO.MonadIO m => FilePath -> m [[FilePath]]
-getFullPaths root = do
-  fullPathsWithRoot <- loop root
-  let
-    rootSplit :: [FilePath] = FilePath.splitPath root
-    splitFullPathsWithRoot :: [[FilePath]] = map FilePath.splitPath fullPathsWithRoot
-    splitFullPathsWithoutRoot :: [[FilePath]] = map (\splitFullPathWithRoot -> splitFullPathWithRoot List.\\ rootSplit) splitFullPathsWithRoot
-  pure splitFullPathsWithoutRoot
-  where
-    loop :: IO.MonadIO m => FilePath -> m [FilePath]
-    loop root' = do
-      rootContents <- IO.liftIO $ Directory.listDirectory root'
-      tree <- Monad.forM rootContents $ \content -> do
-        case FilePath.takeExtension content of
-          "" -> loop $ root' FilePath.</> content
-          _ -> pure [root' FilePath.</> content]
-      pure $ concat tree
-
-getAST :: IO.MonadIO m => FilePath -> m (Exts.ParseResult (Exts.Exp Exts.SrcSpanInfo))
-getAST hspDirectory = do
-  currentDirectory <- IO.liftIO $ Directory.getCurrentDirectory
-  let hspDirAbsPath = currentDirectory FilePath.</> hspDirectory
-  fullPaths <- IO.liftIO $ getFullPaths hspDirAbsPath
-  fullPathsWithParseResult <- IO.liftIO $ Monad.forM fullPaths $ \fullPath -> do
-    fileContents <- readFile $ hspDirAbsPath FilePath.</> (FilePath.joinPath fullPath)
-    let indentedFileContents = unlines $ map (\line -> "  " <> line) $ lines fileContents
-        doBlock = "do\n" <> indentedFileContents
-    pure (fullPath, Exts.parseExpWithMode (Exts.defaultParseMode {Exts.extensions = [Exts.EnableExtension Exts.TypeApplications, Exts.EnableExtension Exts.BlockArguments]}) doBlock)
-  let (_, someExp) = head fullPathsWithParseResult
-  pure someExp
-
-generateParser' :: FilePath -> TH.Q String
-generateParser' hspDirectory = do
-  currentDirectory <- IO.liftIO $ Directory.getCurrentDirectory
-  let hspDirAbsPath = currentDirectory FilePath.</> hspDirectory
-  fullPaths <- IO.liftIO $ getFullPaths hspDirAbsPath
-  fullPathsWithExp :: [([FilePath], TH.Exp)] <- IO.liftIO $ Monad.forM fullPaths $ \fullPath -> do
-    fileContents <- readFile $ hspDirAbsPath FilePath.</> (FilePath.joinPath fullPath)
-    let indentedFileContents = unlines $ map (\line -> "  " <> line) $ lines fileContents
-        doBlock = "do\n" <> indentedFileContents
-    putStrLn doBlock
-    case parseExp doBlock of
-      Left _ -> error "The do block isn't formatted correctly!"
-      Right exp -> pure (fullPath, exp)
-  let (_, someExp) = head fullPathsWithExp
-      pretty = TH.pprint someExp
-  pure pretty
-
--- TODO: DO REMAINDER OF WORK HERE
-generateParser :: FilePath -> TH.Q TH.Exp
-generateParser hspDirectory = do
-  currentDirectory <- IO.liftIO $ Directory.getCurrentDirectory
-  let hspDirAbsPath = currentDirectory FilePath.</> hspDirectory
-  fullPaths <- IO.liftIO $ getFullPaths hspDirAbsPath
-  fullPathsWithExp :: [([FilePath], TH.Exp)] <- IO.liftIO $ Monad.forM fullPaths $ \fullPath -> do
-    fileContents <- readFile $ hspDirAbsPath FilePath.</> (FilePath.joinPath fullPath)
-    let
-      indentedFileContents = unlines $ map (\line -> "  " <> line) $ lines fileContents
-      doBlock = "do/n" <> indentedFileContents
-    case parseExp doBlock of
-      Left _ -> error "The do block isn't formatted correctly!"
-      Right exp@(TH.DoE stmts) -> do
-        let
-          prefixStmts :: [TH.Stmt] = fullPath <&> \path ->
-            if isPathParam $ FilePath.dropExtension path
-            then undefined
-            else undefined
-        pure (fullPath, exp)
-      _ -> error "This isn't a do block!"
-  undefined
-
--- isDirectory :: FilePath -> Bool
--- isDirectory filePath =
---   if (dropWhile ('/' /=) filePath) == "/"
---     then True
---     else False
--}
