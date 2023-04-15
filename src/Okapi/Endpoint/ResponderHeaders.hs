af9c3d749f91c9dcf98122c5e8ebb603eb157875
Rashad Gover <rashad.sasaki@gmail.com>
2023-04-15 22:28:15 +0000

    Add ResponderHeaders and Params modules

---
+++ src/Okapi/Endpoint/ResponderHeaders.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE LinearTypes #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+
+module Okapi.Endpoint.ResponderHeaders where
+
+import qualified Data.Aeson as Aeson
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Builder as Builder
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.List as List
+import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text
+import qualified Network.HTTP.Types as HTTP
+import qualified Web.Cookie as Web
+import qualified Web.HttpApiData as Web
+
+data Error
+  = ParseFail
+  | ParamNotFound
+  | CookieHeaderNotFound
+  | CookieNotFound
+  | ResponderHeadersError -- TODO: ResponderHeaders shouldn't be able to fail...
+  deriving (Eq, Show)
+
+data Response = Response
+  { status :: HTTP.Status,
+    headers :: [ResponseHeader],
+    body :: LBS.ByteString
+  }
+
+data ResponseHeader = ResponseHeader HTTP.HeaderName BS.ByteString
+
+data ResponderHeaders a where
+  FMap :: (a -> b) -> ResponderHeaders a -> ResponderHeaders b
+  Pure :: a -> ResponderHeaders a
+  Apply :: ResponderHeaders (a -> b) -> ResponderHeaders a -> ResponderHeaders b
+  HasHeader :: Web.ToHttpApiData a => HTTP.HeaderName -> ResponderHeaders (a -> (Response -> Response))
+
+eval ::
+  ResponderHeaders a ->
+  () ->
+  (Either Error a, ())
+eval op state = case op of
+  FMap f opX ->
+    case eval opX state of
+      (Left e, state') -> (Left e, state')
+      (Right x, state') -> (Right $ f x, state')
+  Pure x -> (Right x, state)
+  Apply opF opX -> case eval opF state of
+    (Right f, state') -> case eval opX state' of
+      (Right x, state'') -> (Right $ f x, state'')
+      (Left e, state'') -> (Left e, state'')
+    (Left e, state') -> (Left e, state')
+  HasHeader headerName ->
+    let f value response = response {headers = headers response <> [ResponseHeader headerName $ Web.toHeader value]}
+     in (Right f, state)
---
+++ src/Okapi/Params.hs
@@ -0,0 +1,10 @@
+module Okapi.Params where
+
+data Params p q h b r = Params
+  { {-method :: HTTP.Method,-}
+    path :: p,
+    query :: q,
+    headers :: h,
+    body :: b,
+    responder :: r
+  }
